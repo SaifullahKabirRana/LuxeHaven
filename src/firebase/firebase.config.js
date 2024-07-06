9abd759031719515694fd77d7954137b5bb47665
SaifullahKabirRana <saifullahkabirranaa@gmail.com>
2024-07-07 00:41:48 +0600

    complete react project folder structure and router setup

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyARtVEqmQHPbCjFkydR5daHeM8jo3suH5M",
+    authDomain: "luxehaven-a981a.firebaseapp.com",
+    projectId: "luxehaven-a981a",
+    storageBucket: "luxehaven-a981a.appspot.com",
+    messagingSenderId: "280824284440",
+    appId: "1:280824284440:web:d308de6b60a3011abe0a6d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
