c5197ee3fc00c306c645370c7c6705356e2ea695
SaifullahKabirRana <saifullahkabirranaa@gmail.com>
2024-07-11 04:01:03 +0600

    added modal in update profile &update form

--- src/components/UpdateProfile.jsx
+++ src/components/UpdateProfile.jsx
@@ -18,8 +18,48 @@ const UpdateProfile = () => {
                         </div>
 
                     </div>
-                    <div className="flex justify-center mt-2 md:mt-4">
+                    {/* <div className="flex justify-center mt-2 md:mt-4">
                         <button className="btn text-[#00b795] btn-outline border-[#00b795] px-5 text-[15px] md:text-xl"><FaRegEdit className="text-xl md:text-2xl" />Update</button>
+
+                    </div> */}
+                    <div className="flex justify-center mt-2 md:mt-4">
+                        {/* You can open the modal using document.getElementById('ID').showModal() method */}
+                        <button onClick={() => document.getElementById('my_modal_3').showModal()}><button className="btn text-[#00b795] btn-outline border-[#00b795] px-5 text-[15px] md:text-xl"><FaRegEdit className="text-xl md:text-2xl" />Update</button></button>
+                        <dialog id="my_modal_3" className="modal">
+                            <div className="modal-box">
+                                <form method="dialog">
+                                    {/* if there is a button in form, it will close the modal */}
+                                    <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+                                </form>
+                                <div>
+                                    <div className="px-4">
+                                        <form className="">
+                                            <div className="form-control">
+                                                <label className="label">
+                                                    <span className="label-text text-[#403F3F] font-semibold text-[16px] md:text-[20px]">Name</span>
+                                                </label>
+                                                <input type="name" name="name" placeholder="Name" className="input input-bordered"
+                                                defaultValue={user.displayName} required />
+                                            </div>
+                                            <div className="form-control">
+                                                <label className="label">
+                                                    <span className="label-text text-[#403F3F] font-semibold text-[16px] md:text-[20px]">Photo URL</span>
+                                                </label>
+                                                <input type="text" name="photo" placeholder="Photo URL" 
+                                                defaultValue={user.photoURL}
+                                                className="input input-bordered"
+                                                />
+
+                                            </div>
+                                            <div className="form-control mt-6">
+                                                <button className="btn bg-[#00b795] text-white text-[16px] md:text-[20px] font-semibold">Update</button>
+                                            </div>
+                                        </form>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </dialog>
                     </div>
                 </div>
             </div>
